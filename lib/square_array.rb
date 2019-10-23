4c58becd54ab74e2cca0f7c4aca936f95c58c51b
Valentine Maillard <valentinemaillard1@gmail.com>
2019-10-23 21:17:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  count = 0
-  while count < array.length do
-    array
-    count ** 2
+  i = 0
+  while i < array.length do
+    p array
+    i ** 2
     break
   end
 end
